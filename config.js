4c085cb1ad7e6e99fbe2597d31d56cf9cc479888
Frank55-empe <159260196+Frank55-empe@users.noreply.github.com>
2026-07-03 11:03:52 -0300

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbycdZOUrAI2-UT5_is9tsSBuApa33O9PLrtq7vgK7_MvJ-YXE8AUJPWnQc4DCrfCZCZ/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbxBAHYcxCwXruOXIuneZSkxypLN_LKNGWKQbZcQZwRaS0TIor8FJB3qnC18x1cbkrPX/exec"
 };
